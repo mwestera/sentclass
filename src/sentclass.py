c183b053c48b59b4d0a5f967ee6b16cf083d6522
mwestera <m.westera@hum.leidenuniv.nl>
2024-07-03 13:59:50 +0200

    add sentiment; change models; avoid spacy

--- src/sentclass.py
+++ src/sentclass.py
@@ -1,82 +1,107 @@
 import collections
+import logging
+import subprocess
 import sys
 import argparse
 
 import csv
 import re
+import functools
 
 from importlib import resources
+
+from transformers import pipeline
 
 
 """
 Classif sentences along various dimensions.
 
-Currently computes subjectivity based on a transformer model; concreteness simply by averaging word scores from a lookup table (Brysbaert et al.) 
+Currently computes subjectivity, sentiment based on a transformer model; concreteness simply by averaging word scores from a lookup table (Brysbaert et al.) 
 
 Example:
 $ sentclass "This is a sentence" --subj --conc
 
 """
 
+
+_subjectivity_models = {
+    'en': 'GroNLP/mdebertav3-subjectivity-multilingual',    # also for ge, tu, ar
+    # 'en': "cffl/bert-base-styleclassification-subjective-neutral",  # for English only
+    'nl': 'GroNLP/mdebertav3-subjectivity-multilingual',
+    'it': 'GroNLP/mdebertav3-subjectivity-multilingual',
+}
+_sentiment_models = {
+    'en': "cardiffnlp/twitter-xlm-roberta-base-sentiment", # Ar, En, Fr, De, Hi, It, Sp, Pt
+    'fr': "cardiffnlp/twitter-xlm-roberta-base-sentiment",
+    'it': "cardiffnlp/twitter-xlm-roberta-base-sentiment",
+    'nl': "nlptown/bert-base-multilingual-uncased-sentiment", # English, Dutch, German, French, Spanish, and Italian
+}
+
 def main():
 
-    parser = argparse.ArgumentParser(description='Classify sentences along various dimensions.')
-    parser.add_argument('sentence', nargs='?', type=str, default=None, help='Sentence to process; otherwise read lines from stdin.')
-    parser.add_argument('--subj', '--subjectivity', action='store_true', help='Whether to output subjectivity scores.')
-    parser.add_argument('--conc', '--concreteness', action='store_true', help='Whether to output concreteness scores.')
-    # parser.add_argument('--sent', '--sentiment', action='store_true', help='Whether to output sentiment scores.')
+    args = parse_args()
+    classifiers = [_classifier_map[key](args.lang) for key in args.classifiers]
 
-    classifier_map = {'subj': make_subjectivity_classifier,
-                      'conc': make_concreteness_classifier,
-                      # 'sent': make_sentiment_classifier
-                      }
+    input_reader = [args.sentence] if args.sentence else sys.stdin
 
-    args = parser.parse_args(namespace=OrderedNamespace())
-
-    lines = [args.sentence] if args.sentence else sys.stdin
-
-    classifiers_requested = [key for key, val in args.ordered() if val and key in classifier_map] or classifier_map.keys()
-    classifier_functions = [classifier_map[key]() for key in classifiers_requested]
     csvwriter = csv.writer(sys.stdout)
 
-    for line in lines:
-        line = line.strip()
-        csvwriter.writerow(classifier(line) for classifier in classifier_functions)
+    if args.header:
+        csvwriter.writerow(args.classifiers)
+
+    for s in input_reader:
+        s = s.strip()
+        csvwriter.writerow(classifier(s) for classifier in classifiers)
 
 
-class OrderedNamespace(argparse.Namespace):
+def parse_args():
+    parser = argparse.ArgumentParser(description='Classify sentences along various dimensions.')
+    parser.add_argument('sentence', nargs='?', type=str, default=None,
+                        help='Sentence to process; otherwise read lines from stdin.')
+    parser.add_argument('--lang', '--language', type=str, default='en', help='Language code (e.g., en, nl, fr, it)')
+    parser.add_argument('--header', action='store_true', default=None,
+                        help='Where to print a csv-style header first.')
+    for key, func in _classifier_map.items():
+        parser.add_argument(f'--{key}', action='append_const', dest='classifiers', const=key, help=func.__doc__)
+
+    # TODO: If language not provided, auto-detect, maybe using fastlang or langdetect https://github.com/Mimino666/langdetect
+
+    args = parser.parse_args()
+    args.classifiers = ((seen := set())         # deduplicating a list maintaining order :D
+                        or [c for c in args.classifiers if not (c in seen or seen.add(c))]
+                        or _classifier_map.keys())
+
+    return args
+
+
+def make_subjectivity_classifier(language):
     """
-    From https://stackoverflow.com/a/59002780
+    To output subjectivity scores.
     """
-    def __init__(self, **kwargs):
-        self.__dict__["_order"] = []
-        super().__init__(**kwargs)
-    def __setattr__(self, attr, value):
-        super().__setattr__(attr, value)
-        if attr in self._order:
-            self.__dict__["_order"].clear()
-        self.__dict__["_order"].append(attr)
-    def ordered(self):
-        return ((attr, getattr(self, attr)) for attr in self._order)
 
-
-def make_subjectivity_classifier():
-
-    from transformers import pipeline
+    model_name = _subjectivity_models[language]
 
     model = pipeline(
         task="text-classification",
-        model="cffl/bert-base-styleclassification-subjective-neutral",
+        model=model_name,
+        tokenizer=model_name,
         top_k=None,
     )
 
+    index = ['LABEL_0', 'LABEL_1'].index    # TODO: Model-dependent...
+
     def classify_subjectivity(text):
-        return model(text)[0][0]['score']
+        result = model(text)[0]
+        result = sorted(model(text)[0], key=lambda x: index(x['label']))
+        return result[1]['score']
 
     return classify_subjectivity
 
 
-def make_concreteness_classifier():
+def make_concreteness_classifier(language):
+    """
+    To output concreteness scores.
+    """
 
     # TODO: Update with MWE? https://osf.io/preprints/psyarxiv/m397u
 
@@ -90,18 +115,40 @@ def make_concreteness_classifier():
     word_re = re.compile(r'\b\w+\b')
 
     def classify_concreteness(text):
-        word_ratings = [concreteness_ratings.get(w, None) for w in word_re.findall(text.lower())]
+        # TODO: Use lemmatization, getting rid of stop words etc.
+        word_ratings = list(filter(None, (concreteness_ratings.get(w, None) for w in word_re.findall(text.lower()))))
         if not word_ratings:
             return None
-        return sum(filter(lambda x: x is not None, word_ratings)) / len(word_ratings)
+        return (sum(word_ratings) / len(word_ratings) - 1) / 5
 
     return classify_concreteness
 
 
-def make_sentiment_classifier():
+def make_sentiment_classifier(language):
+    """
+    To output sentiment scores.
+    """
 
-    raise NotImplementedError
+    model_name = _sentiment_models[language]
+    model = pipeline("sentiment-analysis", model=model_name, tokenizer=model_name, top_k=None)
 
+    labels = ['negative', 'neutral', 'positive']
+    if language == 'nl':
+        labels = ['1 star', '2 stars', '3 stars', '4 stars', '5 stars'] # TODO: Model-dependent...
+    values = (-1 + (n * 2/(len(labels)-1)) for n in range(len(labels)))
+
+    def classify_sentiment(text):
+        result = model(text)[0]
+        probabilities = [r['score'] for r in sorted(result, key=lambda x: labels.index(x['label']))]
+        aggregate = sum(value * prob for prob, value in zip(probabilities, values))
+        return aggregate
+
+    return classify_sentiment
+
+
+_classifier_map = {'subj': make_subjectivity_classifier,
+                   'conc': make_concreteness_classifier,
+                   'sent': make_sentiment_classifier}
 
 
 if __name__ == '__main__':
